c2fcfefc093cc15068f908fbf3f4996b000eb501
houssni <houssni.mlinde@gmail.com>
2018-03-22 17:36:02 +0100

    config theme OK, ajout module OK

--- lsdt-by-hme/src/app/app.module.ts
+++ lsdt-by-hme/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { ExercicesComponent } from './exercices/exercices.component';
 import { SeancesComponent } from './seances/seances.component';
 import { ProgrammesComponent } from './programmes/programmes.component';
-import {CardModule} from 'primeng/card';
-
+import {ButtonModule} from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { GalleriaModule } from 'primeng/galleria';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,10 @@ import {CardModule} from 'primeng/card';
     ProgrammesComponent
   ],
   imports: [
+    AngularFontAwesomeModule,
+    ButtonModule,
+    CardModule,
+    GalleriaModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
